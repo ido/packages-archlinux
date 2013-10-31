6714dc51cd676cc8559fa64c644f16607a2fd4ae
Ido Rosen <code@idorosen.com>
2013-10-31 13:53:20 -0400

    Updated to 0.6.5

--- aur/lxc-docker/build.sh
+++ aur/lxc-docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 sudo -u vagrant yaourt -Syu --noconfirm
 cd /vagrant
-sudo -u vagrant yaourt -S --noconfirm rsync
-sudo -u vagrant yaourt -S --noconfirm aufs3
-sudo -u vagrant yaourt -S --noconfirm lxc bridge-utils
+sudo -u vagrant yaourt -S --noconfirm --needed rsync
+sudo -u vagrant yaourt -S --noconfirm --needed aufs3
+sudo -u vagrant yaourt -S --noconfirm --needed lxc bridge-utils
 sudo -u vagrant makepkg -s
